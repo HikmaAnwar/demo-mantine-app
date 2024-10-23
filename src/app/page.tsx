2664e7bc017a299c2ced1e555e3b20b55fda30af
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-10-23 15:53:16 +0300

    fix: minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
 "use client";
-import Link from "next/link";
-import { Button } from "@mantine/core";
+import { FileInput, FileInputProps, Pill } from "@mantine/core";
+
+const ValueComponent: FileInputProps["valueComponent"] = ({ value }) => {
+  if (value === null) {
+    return null;
+  }
+
+  if (Array.isArray(value)) {
+    return (
+      <Pill.Group>
+        {value.map((file, index) => (
+          <Pill key={index}>{file.name}</Pill>
+        ))}
+      </Pill.Group>
+    );
+  }
+
+  return <Pill>{value.name}</Pill>;
+};
 
 function Demo() {
   return (
-    <Link href="/hello" passHref>
-      <Button component="a">Next link button1</Button>
-    </Link>
+    <FileInput
+      label="Upload files"
+      placeholder="Upload files"
+      multiple
+      valueComponent={ValueComponent}
+    />
   );
 }
 export default Demo;
